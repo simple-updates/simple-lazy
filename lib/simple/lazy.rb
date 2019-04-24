7aad043d84e43c557cb7b0593b236210c0e200cc
localhostdotdev <localhostdotdev@protonmail.com>
2019-04-24 17:06:28 +0200

    init

---
+++ lib/simple/lazy.rb
@@ -0,0 +1,8 @@
+require "simple/lazy/version"
+
+module Simple
+  module Lazy
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/simple/lazy/version.rb
@@ -0,0 +1,5 @@
+module Simple
+  module Lazy
+    VERSION = "0.1.0"
+  end
+end
